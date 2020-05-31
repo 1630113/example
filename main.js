042d88edb7f8483022fc71d550386759a82994c8
1630113 <63180367+1630113@users.noreply.github.com>
2020-05-31 18:02:16 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,13 @@
 
-function ready() {
+    //Javascript Code
+    const val = document.querySelector('.value');
+const kelvin = document.querySelector('#k');
+const fahrenheit = document.querySelector('#f');
+// console.log(fahrenheit);
 
-    const total = 3;
-    let score = 0;
-    const res = document.querySelector('.result');
-
-
-
-    //get the data
-    const q1 = document.forms['quiz']["q1"].value;
-
-    const q2 = document.forms['quiz']['q2'].value;
-
-    const q3 = document.forms['quiz']['q3'].value;
-    const Fa = document.querySelector('.Fa');
-    // console.log(fa);
-
-    //validation
-    for (i = 1; i <= total; i++) {
-        if (eval('q' + i) == null || eval('q' + i) == '') {
-            alert("you have missed the question" + i);
-            return false;
-
-        }
-
-    }
-    //set answer
-    const ans = ['a', 'b', 'b'];
-    for (i = 1; i <= total; i++) {
-        if (eval('q' + i) == ans[i - 1]) {
-            score++;
-        }
-
-    }
-    alert(score);
-    Fa.innerHTML = score;
-    return false;
-}
+// Event organise
+val.addEventListener('input',function(){
+//   console.log("hii");
+fahrenheit.innerHTML = parseInt(val.value)*(9/5)+32;
+kelvin.innerHTML = parseInt(val.value)+273.15;
+});
